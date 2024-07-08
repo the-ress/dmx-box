509b802bc87a1cf63078027e4ffe5b7710666828
Julie Koubova <julie@koubova.net>
2024-07-08 21:03:17 +0200

    container

---
+++ container/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+command -v podman >/dev/null && DOCKER=podman
+
+: "${DOCKER:=docker}
+: "${IDF_CLONE_BRANCH_OR_TAG:=release/v5.2}
+: "${IDF_INSTALL_TARGETS:=esp32}
+: "${IDF_CLONE_SHALLOW:=1}
+
+CDPATH= cd -- "$(dirname -- "$0")"
+$DOCKER build -t dmxbox/espidf -f Dockerfile.espidf \
+  --build-arg "IDF_CLONE_BRANCH_OR_TAG=${IDF_CLONE_BRANCH_OR_TAG}" \
+  --build-arg "IDF_CLONE_SHALLOW=1" \
+  --build-arg "IDF_INSTALL_TARGETS=esp32"
+$DOCKER build -t dmxbox/node -f Dockerfile.node
+$DOCKER build -t dmxbox -f Dockerfile
+
---
+++ container/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+command -v podman >/dev/null && DOCKER=podman
+
+: "${DOCKER:=docker}"
+: "${DMXBOX_DEVICE:=/dev/ttyUSB0}"
+: "${DMXBOX_MOUNTPOINT:=/home/ubuntu/project}"
+
+CDPATH= cd -- "$(dirname -- "$0")"/..
+exec $DOCKER run \
+  --rm \
+  --interactive \
+  --tty \
+  --volume ".:${DMXBOX_MOUNTPOINT}" \
+  --device "${DMXBOX_DEVICE}" \
+  --userns=keep-id \
+  --group-add keep-groups \
+  dmxbox
+  "$@"
