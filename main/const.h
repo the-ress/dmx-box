155b362e603d6bc3f574fdf966b97e1b97325ab4
Julie Koubova <julie@koubova.net>
2024-07-09 14:27:27 +0000

    clean

--- main/const.h
+++ main/const.h
@@ -1,14 +1,6 @@
 #include "sdkconfig.h"
 #include "esp_dmx.h"
 
-// #define PWR_GPIO CONFIG_PWR_GPIO
-#define POWER_LED_GPIO 18
-#define DMX_OUT_LED_GPIO 14
-#define DMX_IN_LED_GPIO 19
-#define AP_MODE_LED_GPIO 22
-#define CLIENT_MODE_LED_GPIO 27
-#define ARTNET_IN_LED_GPIO 21
-
 #define RESET_BUTTON_GPIO 2
 #define WIFI_MODE_GPIO 4
 
