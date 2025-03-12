a3a669409436b684d2c7d4200f81ea6d076dbfb8
Tereza Tomcova <tereza.tomcova@gmail.com>
2025-03-12 21:48:39 +0100

    test all leds on startup

--- components/dmxbox_led/dmxbox_led.c
+++ components/dmxbox_led/dmxbox_led.c
@@ -1,23 +1,36 @@
-#include "dmxbox_led.h"
 #include <driver/gpio.h>
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <portmacro.h>
+
+#include "dmxbox_led.h"
 
 static const char *TAG = "dmxbox_led";
 
-static void dmxbox_led_start_one(dmxbox_led_t led) {
-  ESP_ERROR_CHECK(gpio_set_level(led, 1));
+static void dmxbox_led_start_one(dmxbox_led_t led, bool level) {
+  ESP_ERROR_CHECK(gpio_set_level(led, !level));
   ESP_ERROR_CHECK(gpio_reset_pin(led));
   ESP_ERROR_CHECK(gpio_set_direction(led, GPIO_MODE_OUTPUT));
 }
 
 void dmxbox_led_start() {
   ESP_LOGI(TAG, "Configuring LED GPIOs");
-  dmxbox_led_start_one(dmxbox_led_dmx_out);
-  dmxbox_led_start_one(dmxbox_led_power);
-  dmxbox_led_start_one(dmxbox_led_dmx_in);
-  dmxbox_led_start_one(dmxbox_led_artnet_in);
-  dmxbox_led_start_one(dmxbox_led_ap);
-  dmxbox_led_start_one(dmxbox_led_sta);
+  dmxbox_led_start_one(dmxbox_led_dmx_out, 1);
+  dmxbox_led_start_one(dmxbox_led_power, 1);
+  dmxbox_led_start_one(dmxbox_led_dmx_in, 1);
+  dmxbox_led_start_one(dmxbox_led_artnet_in, 1);
+  dmxbox_led_start_one(dmxbox_led_ap, 1);
+  dmxbox_led_start_one(dmxbox_led_sta, 1);
+
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+
+  dmxbox_led_set(dmxbox_led_dmx_out, 0);
+  dmxbox_led_set(dmxbox_led_power, 0);
+  dmxbox_led_set(dmxbox_led_dmx_in, 0);
+  dmxbox_led_set(dmxbox_led_artnet_in, 0);
+  dmxbox_led_set(dmxbox_led_ap, 0);
+  dmxbox_led_set(dmxbox_led_sta, 0);
 }
 
 esp_err_t dmxbox_led_set(dmxbox_led_t led, bool level) {
